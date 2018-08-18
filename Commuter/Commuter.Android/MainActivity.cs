df4c8966efe8d9cb620ab2f506b99320f4e82bc1
pentest30 <bikbal118@yahoo.com>
2018-08-18 23:05:18 +0100

    first commit

---
+++ Commuter/Commuter.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Commuter.Droid
+{
+    [Activity(Label = "Commuter", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        //public override async void OnRequestPermissionsResult(int requestCode, string[] permissions,
+        //    [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    await PermissionsImplementation.Current.RequestPermissionsAsync();
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+    }
+}
+
---
+++ Commuter/Commuter/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace Commuter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GpsBtn_OnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 100;
+                var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(20), null, true);
+
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+               // throw;
+            }
+        }
+    }
+}
